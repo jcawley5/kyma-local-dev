b092c8059db370052a1fdeefe05508a3ac531303
Jamie Cawley <jamie.cawley@gmail.com>
2020-01-23 16:48:40 -0500

    adding namespace config

--- build.js
+++ build.js
@@ -5,22 +5,50 @@ const dotenv = require("dotenv");
 
 module.exports = {
   deploymentYaml: function() {
-    const deploymentJSON = JSON.parse(fs.readFileSync("deployment.json"));
-    const packageJSON = JSON.parse(fs.readFileSync("package.json"));
+    const deploymentJSON = JSON.parse(
+      fs.readFileSync("deployment-template.json")
+    );
+    const packageStr = fs.readFileSync("package.json").toString();
+    const packageJSON = JSON.parse(packageStr);
     const lamdbdCode = fs.readFileSync(packageJSON.main).toString();
-    const envVarData = Buffer.from(".env.json");
+    const envVarData = dotenv.parse(fs.readFileSync(".env").toString());
+    const envVars = Object.entries(envVarData);
+    var envArr = [];
+
+    for (const [name, value] of envVars) {
+      let tempObj = {
+        name: name,
+        value: value
+      };
+      envArr.push(tempObj);
+    }
 
     const params = {
       function_name: packageJSON.name,
-      function_size: "XS",
-      environment_variables: dotenv.parse(envVarData),
-      function_deps: packageJSON,
+      function_size: packageJSON.buildParameters.function_size,
+      environment_variables: envArr,
+      function_deps: packageStr,
       function_code: lamdbdCode,
-      function_trigger: packageJSON.function_trigger
+      function_trigger: packageJSON.buildParameters.function_trigger,
+      namespace: packageJSON.buildParameters.namespace
     };
 
-    const deploymentYAML = parameterize(deploymentJSON, params);
+    const horizontalPodAutoscalerJSON = JSON.parse(
+      fs
+        .readFileSync(
+          "horizontalPodAutoscaler_" +
+            packageJSON.buildParameters.function_size +
+            ".json"
+        )
+        .toString()
+    );
 
+    params.horizontalPodAutoscaler = parameterize(
+      horizontalPodAutoscalerJSON,
+      params
+    );
+
+    const deploymentYAML = parameterize(deploymentJSON, params);
     fs.writeFileSync(`deployment.yaml`, yaml.stringify(deploymentYAML));
   }
 };
