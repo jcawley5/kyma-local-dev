7649a8188281da977e9c61d389fcb05f32be35d0
Jamie Cawley <jamie.cawley@sap.com>
2021-01-07 15:21:06 -0500

    test

--- lambdas/examplefn/handler.js
+++ lambdas/examplefn/handler.js
@@ -5,7 +5,7 @@ module.exports = {
     console.log("env variable header 1 - test 1: ", process.env["header1"]);
 
     const Url = "https://httpbin.org/anything";
-    console.log("sending get to ", Url);
+    console.log("sending get request to ", Url);
     const { data: result } = await axios.get(Url, {
       params: { header1: process.env.header1 }
     });
