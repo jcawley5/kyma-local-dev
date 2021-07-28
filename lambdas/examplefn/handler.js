691447ec8735286384df4da575868e67fed09fae
Jamie Cawley <jamie.cawley@sap.com>
2021-07-28 15:43:32 -0400

    Update handler.js

--- lambdas/examplefn/handler.js
+++ lambdas/examplefn/handler.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 module.exports = {
   main: async function(event, context) {
-    console.log("env variable header 1 - test 1: ", process.env["header1"]);
+    console.log("env variable header 1 -- test 1: ", process.env["header1"]);
 
     const Url = "https://httpbin.org/anything";
     console.log("sending get request to ", Url);
